3ca953a9c5ba422ac47466dab7619d23aeae2834
Minghao QU <MQu2@slb.com>
2022-11-03 11:12:34 +0800

    chore: set MySQL configuration and add DI

--- EFCoreRelationshipsPractice/Program.cs
+++ EFCoreRelationshipsPractice/Program.cs
@@ -1,37 +1,40 @@
 using EFCoreRelationshipsPractice.Repository;
+using EFCoreRelationshipsPractice.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<CompanyService>();
+
 builder.Services.AddDbContext<CompanyDbContext>(options =>
 {
-    var connectionString = builder.Configuration.GetConnectionString("Default");
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+  var connectionString = builder.Configuration.GetConnectionString("Default");
+  options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
 var app = builder.Build();
 
-using(var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<CompanyDbContext>();
-    using (var context = scope.ServiceProvider.GetService<CompanyDbContext>())
-    {
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
-    }
+  var dbContext = scope.ServiceProvider.GetRequiredService<CompanyDbContext>();
+  using (var context = scope.ServiceProvider.GetService<CompanyDbContext>())
+  {
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+  }
 }
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
