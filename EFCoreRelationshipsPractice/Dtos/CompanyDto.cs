f48a13df7bdeb546ad2b3c7285025e4018b2346a
xma12 <xma12@slb.com>
2022-11-03 15:13:45 +0800

    feat: add company and profile to db

--- EFCoreRelationshipsPractice/Dtos/CompanyDto.cs
+++ EFCoreRelationshipsPractice/Dtos/CompanyDto.cs
@@ -12,11 +12,12 @@ namespace EFCoreRelationshipsPractice.Dtos
         public CompanyDto(Model.CompanyEntity companyEntity)
         {
             Name = companyEntity.Name;
+            ProfileDto = companyEntity.Profile != null ? new ProfileDto(companyEntity.Profile) : null;
         }
 
         public string Name { get; set; }
 
-        public ProfileDto? Profile { get; set; }
+        public ProfileDto? ProfileDto { get; set; }
 
         public List<EmployeeDto>? Employees { get; set; }
 
@@ -25,6 +26,7 @@ namespace EFCoreRelationshipsPractice.Dtos
             return new CompanyEntity()
             {
                 Name = this.Name,
+                Profile = this.ProfileDto?.ToEntity(),
             };
         }
     }
