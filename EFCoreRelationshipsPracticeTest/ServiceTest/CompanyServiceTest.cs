28d411fbf78e42e9ddf6f051eb6ab147807626a2
Xu DU <XDu3@slb.com>
2022-11-03 17:16:31 +0800

    test: add get all test

--- EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -2,6 +2,15 @@
 {
     public class CompanyServiceTest: CompanyServiceTestBase
     {
+        [Fact]
+        public async void Should_return_2_company_with_2_employee_when_get_all()
+        {
+            var companies = await CompanyService.GetAll();
+            Assert.Equal(2, companies.Count);
+            Assert.Equal("SLB", companies[0].Name);
+            Assert.Equal("SLBCert", companies[0].ProfileDto?.CertId);
+            Assert.Equal(2, companies[0].EmployeeDtos?.Count);
+        }
         
     }
 }
--- EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTestBase.cs
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTestBase.cs
@@ -1,4 +1,5 @@
-﻿using EFCoreRelationshipsPractice.Repository;
+﻿using EFCoreRelationshipsPractice.Models;
+using EFCoreRelationshipsPractice.Repository;
 using EFCoreRelationshipsPractice.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,21 +7,42 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
 {
     public class CompanyServiceTestBase : IDisposable
     {
-        private readonly CompanyService _companyService;
-        private readonly CompanyDbContext _companyDbContext;
+        internal readonly CompanyService CompanyService;
+        internal readonly CompanyDbContext CompanyDbContext;
         public CompanyServiceTestBase()
         {
             var options = new DbContextOptionsBuilder<CompanyDbContext>()
                 .UseInMemoryDatabase(databaseName: "DB")
                 .Options;
 
-            _companyDbContext = new CompanyDbContext(options);
-            _companyService = new CompanyService(_companyDbContext);
+            CompanyDbContext = new CompanyDbContext(options);
+            CompanyService = new CompanyService(CompanyDbContext);
+
+            InitDataBase();
+        }
+
+        private void InitDataBase()
+        {
+            CompanyDbContext.Companies.AddRange(new List<CompanyEntity>()
+            {
+                new CompanyEntity(){Name = "SLB",Employees = GetInitEmployees(), Profile = new ProfileEntity(){CertId = "SLBCert", RegisteredCapital = 1000}},
+                new CompanyEntity(){Name = "Google",Employees = GetInitEmployees(), Profile = new ProfileEntity(){CertId = "GoogleCert", RegisteredCapital = 2000}}
+            });
+            CompanyDbContext.SaveChanges();
+        }
+
+        private List<EmployeeEntity> GetInitEmployees()
+        {
+            return new List<EmployeeEntity>()
+            {
+                new EmployeeEntity() { Age = 20, Name = "Xu" },
+                new EmployeeEntity() { Age = 30, Name = "Du" }
+            };
         }
 
         public void Dispose()
         {
-            _companyDbContext.Dispose();
+            CompanyDbContext.Dispose();
         }
     }
 }
